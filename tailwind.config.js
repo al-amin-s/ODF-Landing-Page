4d2deb6503ec3833d44452d98e9d6d9f5f27c62a
al-amin-s <ala756297@gmali.com>
2024-09-24 09:13:02 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html"], // Add paths where your HTML files are located
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")], // Ensure DaisyUI is included here
+}
